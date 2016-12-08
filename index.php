60f1cc57d96beb5679fbe45f02475b06a7c8017e
Henrique Buzin <henrique_buzin@estudante.sc.senai.br>
2016-12-08 15:59:22 -0200

    Index
    Melhorias na Index

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
     include "config.php";
-
     if(isset($_GET["controller"])){
         $controller = ucfirst(strtolower(addslashes(filter_input(INPUT_GET, 'controller')))) . "Controller";
     }else{
