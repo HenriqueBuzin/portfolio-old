68abe671d124bb96458c7c3c1a59a0b46dae057f
Henrique Buzin <henrique_buzin@estudante.sc.senai.br>
2016-12-08 15:45:59 -0200

    Ajustes para importação
    Posibilita importar

--- index.php
+++ index.php
@@ -9,22 +9,17 @@
     <head>
         <meta charset="utf-8">
         <meta name="author" content="Henrique Antonio Buzin Vargas">
+        <base href="<?php echo BASE; ?>">
         <script src="assets/js/jquery.min.js"></script>
-        <script src="assets/js/jquery.mask.min.js"></script>
-        <script src="assets/js/index.js"></script>
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/css/datatables.min.css">
-        <link rel="stylesheet" href="assets/css/index.css">
     </head>
     <body>
         <?php
-            include_once "header.php";
             if(method_exists($controller, $action)){
                 $controller->$action();
             }else {
                 include_once "admin/view/error/404.php";
             }
-            include_once "footer.php";
         ?>
     </body>
 </html>
